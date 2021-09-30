84c6995272b4fb9e5de681dd035c85f2af2917ff
Treasure Onoja <treasureonoja@Treasures-MacBook-Air.local>
2021-09-30 10:57:30 +0100

    Added unit test

--- src/test/java/com/treasureio/plunt/PluntApplicationTests.java
+++ src/test/java/com/treasureio/plunt/PluntApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PluntApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
---
+++ src/test/java/com/treasureio/plunt/service/DepartmentServiceTest.java
@@ -0,0 +1,44 @@
+package com.treasureio.plunt.service;
+
+import com.treasureio.plunt.entity.Department;
+import com.treasureio.plunt.repository.DepartmentRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class DepartmentServiceTest {
+
+    @Autowired
+    private DepartmentService departmentService;
+
+    @MockBean
+    private DepartmentRepository departmentRepository;
+
+    @BeforeEach
+    void setUp() {
+        Department department =
+                Department.builder()
+                .departmentName("IT")
+                .departmentAddress("Ahmedabad")
+                .departmentCode("IT-06")
+                .departmentId(1L)
+                .build();
+
+        Mockito.when(departmentRepository.findByDepartmentNameIgnoreCase("IT"))
+                .thenReturn(department);
+    }
+
+    @Test
+    public void whenvalidDepartmentName_thenDepartmentShouldFound() {
+        String departmentName = "IT";
+        Department found = departmentService.fetchDepartmentByName(departmentName);
+
+        assertEquals(departmentName, found.getDepartmentName());
+    }
+}
